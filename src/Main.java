5e006e67d2b7aaf48d094d6c0c27bb0bce40e120
Kanuck Shah <76070714+Kanuckshah@users.noreply.github.com>
2026-01-03 18:44:06 -0500

    New Commits

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println("Hello and welcome!");
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+  public static void main(String[] args) {
+    printWelcome();
+    runCounter();
+    printGoodbye();
+  }
+
+  public static void printWelcome() {
+    IO.println("Welcome to the ICS4U Java Final Project");
+    IO.println("--------------------------------------");
+  }
+
+  public static void runCounter() {
+    for (int i = 1; i <= 5; i++) {
+      IO.println("Count: " + i);
+    }
+  }
+
+  public static void printGoodbye() {
+    IO.println("--------------------------------------");
+    IO.println("Program finished successfully.");
   }
 }
